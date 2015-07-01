015f1b3f3d319d44d388397dfdf57d68a2559d77
David Bradshaw <david.bradshaw.usa@gmail.com>
2015-07-01 01:53:56 -0400

    Fixed issue with multiple routes on goals

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   def tag_link_to sym,g
     atrb = GoalsController.actions[sym]
     atrb[:id] = g.id
-    return link_to atrb[:label], {:id => atrb[:id], :controller => atrb[:controller], :action => sym}, :remote => atrb[:remote], :method => atrb[:method]
+    return link_to atrb[:label], {:id => atrb[:id], :workstream_id => g.workstream.id, :controller => atrb[:controller], :action => sym}, :remote => atrb[:remote], :method => atrb[:method]
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,7 @@ Rails.application.routes.draw do
 
   resources :projects, :controller => "workstreams", :type => "Project"
 
-  resources :goals
+  #resources :goals
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
